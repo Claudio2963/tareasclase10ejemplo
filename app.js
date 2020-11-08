80ac3694d32dc33d13c31b37f7e6d789bd03622d
Claudio2963 <claudiovalin2963@gmail.com>
2020-11-08 09:49:54 -0300

    arrancando el trabajo modificando

--- app.js
+++ app.js
@@ -1,4 +1,76 @@
-let modulousuarios = require('./usuarios')
+let moduloUsuarios = require('./usuarios')
 const process = require('process');
 
 let comando = process.argv[2]
+
+switch (comando) {
+    case 'prueba' :
+        console.log('ESTA ES UNA PRUEBA DE LO QUE HACE ARGV')    
+    break
+    case 'listar':
+        let usuarios = moduloUsuarios.leerJSON();
+        console.log('---------------------------------------------')
+        console.log('------------LISTA TE usuarios------------------')
+        console.log('---------------------------------------------')
+        for (let index = 0; index < usuarios.length; index++) {
+            
+           console.log('Titulo de la tarea: ' + usuarios[index].titulo + ', con estado: ' +  usuarios[index].estado)
+        }
+        break;
+    case 'agregar' :
+        let titulo = process.argv[3];
+        let estado = process.argv[4];
+        moduloUsuarios.agregarTarea(titulo,estado)
+        console.log('---------------------------------------------')
+        console.log('------LA TAREA HA SIDO AGREGADA--------------')
+        console.log('---------------------------------------------')
+        break;
+    case 'deshacer':
+        moduloUsuarios.deshacer();
+        console.log('---------------------------------------------')
+        console.log('--------SE ELIMINÓ LA ÚLTIMA TAREA-----------')
+        console.log('---------------------------------------------')
+        break
+    case 'filtrar':
+        let filtro = process.argv[3]
+        let usuariosFiltradas = moduloUsuarios.filtrar(filtro);
+        console.log('---------------------------------------------')
+        console.log('----usuarios FILTRADAS POR ' + filtro.toUpperCase() + '-----------')
+        console.log('---------------------------------------------')
+        usuariosFiltradas.forEach(function(tarea){
+            console.log('Titulo de la tarea: ' + tarea.titulo + ', con estado: ' +  tarea.estado)
+        })
+        break
+    case 'buscar':
+        let busqueda = process.argv[3]
+        let resultadoDeLaBusqueda = moduloUsuarios.buscar(busqueda);
+        if(resultadoDeLaBusqueda.length == 0){
+            console.log('---------------------------------------------')
+            console.log('-----NO HUBIERON RESULTADOS DE ' + busqueda.toUpperCase() +'--------')
+            console.log('---------------------------------------------')
+        }else{
+            console.log('---------------------------------------------')
+            console.log('-----RESULTADO DE LA BUSQUEDA: ' + busqueda.toUpperCase() +'--------')
+            console.log('---------------------------------------------')
+            resultadoDeLaBusqueda.forEach(function(tarea){
+                console.log('Titulo de la tarea: ' + tarea.titulo + ', con estado: ' +  tarea.estado)
+            })
+        }
+        break
+    case undefined :
+        
+        console.log(`Tenés que utilizar alguno de los siguientes comandos:
+        - listar
+        - prueba
+        - deshacer
+        - agregar
+        - filtrar
+        - buscar
+        `)
+        break
+    default:
+        console.log('---------------------------------------------')
+        console.log('-----ESTE COMANDO NO ESTÁ DISPONIBLE---------')
+        console.log('---------------------------------------------')
+        break;
+}
--- usuarios.js
+++ usuarios.js
@@ -3,8 +3,47 @@ const fs = require('fs'); //requiero el modulo file system
 let moduloUsuarios = {
     archivo : './usuarios.json',
     leerJSON : function(){
-        let usuariosJson = fs.readFileSync(this.archivo,'utf-8');
-        let listaDeusuarios = JSON.parse(usuariosJson)
-        return listaDeusuarios
+        let UsuariosJson = fs.readFileSync(this.archivo,'utf-8');
+        let listaDeUsuarios = JSON.parse(UsuariosJson)
+        return listaDeUsuarios
+    },
+    agregarTarea : function(titulo,estado){
+        let listaDeUsuarios = this.leerJSON();
+
+        let nuevaTarea = {
+            titulo : titulo,
+            estado : estado
+        }
+        listaDeUsuarios.push(nuevaTarea)
+        
+        this.guardarJSON(listaDeUsuarios)
+
+    },
+    guardarJSON : function(info){
+        let nuevoJSON = JSON.stringify(info)
+        fs.writeFileSync(this.archivo,nuevoJSON,'utf-8');
+        
+    },
+    deshacer : function(){
+        let listaDeUsuarios = this.leerJSON();
+        listaDeUsuarios.pop();
+        
+        this.guardarJSON(listaDeUsuarios);
+    },
+    filtrar : function(filtro){
+        let listaDeUsuarios = this.leerJSON()
+        let UsuariosFiltradas = listaDeUsuarios.filter(function(tarea) {
+            return tarea.estado === filtro
+        })
+        return UsuariosFiltradas
+    },
+    buscar : function(busqueda){
+        let listaDeUsuarios = this.leerJSON();
+        let resultadoDeLaBusqueda = listaDeUsuarios.filter(function(tarea) {
+            return tarea.titulo.includes(busqueda)
+        })
+        return resultadoDeLaBusqueda
     }
 }
+
+module.exports = moduloUsuarios
